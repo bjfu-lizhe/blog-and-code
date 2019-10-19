2399db0d9a3a186de44ee7c2beb941ba24e5c5ea
bjfu-lizhe <56750846+bjfu-lizhe@users.noreply.github.com>
2019-10-19 16:11:30 +0800

    stm32f103t8u6_icm20948.h

---
+++ stm32f103t8u6_icm20948.h
@@ -0,0 +1,11 @@
+#ifndef __driver_h
+#define __driver_h
+
+#include "stm32f10x.h"
+#include "delay.h"
+
+unsigned char icm20948_init(void);
+
+unsigned char icm20948_get(short *buf, const int length);
+
+#endif
